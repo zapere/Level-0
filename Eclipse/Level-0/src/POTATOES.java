02137d07b3aa4a9bbd241481c4d278b0fb55b322
Elijah <league@Mac-mini.local>
2016-08-04 18:27:56 -0700

    Breaking Phone Codes and Making some potato cartoon lyrics

---
+++ Eclipse/Level-0/src/POTATOES.java
@@ -0,0 +1,15 @@
+
+public class POTATOES {
+	public static void main(String[] args) {
+		System.out.println("1 Potato");
+		for (int i = 2; i < 3; i = i + 1) {
+			System.out.println(i + " Potatoes");
+		}
+		System.out.println("4");
+		for (int j = 5; j < 9; j = j + 1) {
+			System.out.println(j + " Potatoes");
+		}
+		System.out.println("MOREEEE!");
+
+	}
+}
