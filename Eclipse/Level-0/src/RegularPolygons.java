5fe1c41cfa93277d824067a2850fbe8639fe57ce
Elijah <league@Mac-mini-3.local>
2016-10-06 17:55:26 -0700

    Making Shapes better than ever

--- Eclipse/Level-0/src/RegularPolygons.java
+++ Eclipse/Level-0/src/RegularPolygons.java
@@ -5,43 +5,56 @@ public class RegularPolygons {
 	public static void main(String[] args) {
 		Robot robot = new Robot();
 		robot.setSpeed(100);
-		robot.moveTo(500, 500);
 		robot.penDown();
 		robot.setPenColor(Colors.getRandomColor());
-		int sides = 4;
-		int sides2 = 3;
-		float angle = getAngle(sides);
-		System.out.println(angle);
-		for(int i=0;i<sides;i=i+1){
-			robot.setPenColor(Colors.getRandomColor());
-			robot.move(100);
-			robot.turn((int) (180-angle));
-			
-		}
-		robot.turn(90);
-		robot.move(50);
-		robot.turn(90);
-		robot.penUp();
-		robot.move(100);
-		robot.penDown();
-		for(int i=0;i<sides2;i=i+1){
-			robot.setRandomPenColor();
-			robot.move(100);
-			robot.turn((int) (180-getAngle(sides2)));
-		}
-//		robot.turn(-90);
-//		robot.move(300);
-		robot.moveTo(00, 00);
-		
+		drawShape(robot,600,500,24,20);
+//	    int sides = 4;
+//		int sides2 = 3;
+//		drawShape(robot, 500, 500, 4, 100);
+//		float angle = getAngle(sides);
+//		System.out.println(angle);
+//		// for(int i=0;i<sides;i=i+1){
+//		// robot.setPenColor(Colors.getRandomColor());
+//		// robot.move(100);
+//		// robot.turn((int) (180-angle));
+//		//
+//		// }
+//		// robot.turn(90);
+//		// robot.move(50);
+//		// robot.turn(90);
+//		// robot.penUp();
+//		// robot.move(100);
+//		// robot.penDown();
+//		// for(int i=0;i<sides2;i=i+1){
+//		// robot.setRandomPenColor();
+//		// robot.move(100);
+//		// robot.turn((int) (180-getAngle(sides2)));
+//		// }
 
 	}
-	public static float getAngle(int sides){
-		float total = sides*180-360;
-		float angle = total/sides;
+
+	
+
+	public static double getAngle(int sides) {
+		double total = sides * 180 - 360;
+		double angle = total / sides;
 		return angle;
 	}
-	public static void drawShape(Robot robot, int x,int y,int sides,int length){
-		robot.moveTo(500, 500);
+
+	public static void drawShape(Robot robot, int x, int y, int sides, int length) {
+		robot.penUp();
+		robot.moveTo(600, 512);
+		robot.penDown();
+		robot.setPenColor(Colors.getRandomColor());
+		int sides2 = 3;
+		double angle = getAngle(sides);
+		System.out.println(angle);
+		for (int i = 0; i < sides; i = i + 1) {
+			robot.setPenColor(Colors.getRandomColor());
+			robot.move(20);
+			robot.turn((int) (180.0 - angle));
+
 		}
-	
+	}
+
 }
