4d0fcd489a4efc89b8545da7d5107d39d34758d0
BaCoNmAn666 <league@Mac-mini-2.local>
2017-02-16 18:31:09 -0800

    Printing the biggest palindrome with 3 digit numbers and what the factors are

--- Eclipse/Level-0/src/Problem4.java
+++ Eclipse/Level-0/src/Problem4.java
@@ -1,14 +1,24 @@
 
-public class Problem3 {
+public class Problem4 {
 	public static void main(String[] args) {
+		int biggestNumber = 0;
+		int a = 0;
+		int b = 0;
+		
 		for (int first = 100; first < 1000; first++) {
 			for(int second = 100; second < 1000; second++){
 				int Product = first*second;
 				if(isPalindrome(Product)){
 					System.out.println(""+Product);
+					if(Product>biggestNumber){
+						biggestNumber=Product;
+						a=first;
+						b=second;
+					}
 				}
 			}
 		}
+		System.out.println("The biggest palindrome is "+biggestNumber+" which is "+a+" * "+b);
 	}
 
 	public static boolean isPalindrome(int n) {
